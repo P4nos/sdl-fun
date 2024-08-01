1e5c3845375c775a8c222c5e72a9644da26ac3ea
p4nos <p4nos>
2024-08-01 11:51:09 +0200

    add simulation reset

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
-#include "draw.h"
+#include "defs.h"
 #include "state.h"
 
 State state;
 
 int main() {
+  srand(time(NULL));
+
   if (setup() > 0) {
     exit(-1);
   };
@@ -16,16 +21,26 @@ int main() {
   SDL_Event e;
   bool running = true;
   while (running) {
-
-    clear_screen();
     // event loop
     while (SDL_PollEvent(&e) != 0) {
       switch (e.type) {
       case SDL_QUIT:
         running = false;
         break;
+      case SDL_KEYDOWN:
+        switch (e.key.keysym.sym) {
+        case SDLK_j:
+          update_state();
+          break;
+        case SDLK_r:
+          reset_state();
+          init_state();
+          break;
+        }
       }
     }
+
+    clear_screen();
     // physics loop
     update_state();
     // render loop
